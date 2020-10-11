cb8e5e4f6b5591ced47b5e5cfb48fae91ee76306
German Rodrigo <gerorean@gmail.com>
2020-10-11 16:28:52 -0500

    2.4.2b Mongoose part 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//2.4.1
+//2.4.1 -2.4.2
 //Modules
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -9,27 +9,25 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
     //New
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
+    Dishes.create({
+        name: 'Uthapizza',
+        description: 'Test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        //Find
+        return Dishes.find({});
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        //Remove
+        return Dishes.remove({});
+    })
+    .then(() => {
+        //Close
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-    //Save
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-            //Find
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-            //Remove
-            return Dishes.remove({});
-        })
-        .then(() => {
-            //Close
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
